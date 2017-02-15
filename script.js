aeb813b88560f0a497a18951fba8c4b676b2258f
Stella Kim <stellajkimm@gmail.com>
2017-02-15 04:58:57 +0000

    create tiny turtle repo

---
+++ script.js
@@ -0,0 +1,11 @@
+TinyTurtle.apply(window); 
+
+// Write directions for the tiny turtle to make
+// the shape of a house by using the following methods below:
+//
+// forward();
+// right();
+// left();
+// stamp();
+//
+
